6930accbfd31ec000da13911ff4da9431f21d484
Avnish-oP <kavnish1245@gmail.com>
2024-06-26 12:00:48 +0530

    gradient text added

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
      <div className="min-h-screen flex items-center justify-center ">
       <div className="w-full max-w-md mx-auto p-6 bg-white rounded-xl shadow-lg">
-        <h1 className="text-3xl font-bold mb-4 text-center text-gray-800">Stylish To-Do App</h1>
+        <h1 className="text-3xl font-bold mb-4 text-center bg-gradient-to-r from-purple-500 to-indigo-500 inline-block text-transparent bg-clip-text brightness-125 backdrop-contrast-150">Stylish To-Do App</h1>
         <TaskInput />
         <TaskList />
       </div>
